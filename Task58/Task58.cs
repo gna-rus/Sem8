537fdbfa790d06c1f1a5e5e6f7e141abe1c0a0dc
Nickolay Grigoriev <gnarus@inbox.ru>
2023-01-27 22:01:26 +0300

    first commit for Task58

---
+++ Task58/Task58.cs
@@ -0,0 +1,67 @@
+﻿/*Задача 58: Задайте две матрицы. Напишите программу, которая будет находить произведение двух матриц.
+Например, даны 2 матрицы:
+2 4 | 3 4
+3 2 | 3 3
+Результирующая матрица будет:
+18 20
+15 18*/
+
+Console.Clear();
+
+int[,] FillMatrix(int[,] matr) // метод заполнения  матрицы случайно сгенерированными числами
+{
+    Random rnd = new Random();
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i, j] = rnd.Next(0, 11); // присвоение элементу матрицы значения от 0 до 10            
+        }
+    }
+    return matr;
+}
+
+void PrintMatrix(int[,] matr) // метод вывода на экран матрицы
+{
+    
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            Console.Write($"{matr[i, j]} "); // вывод на экран результата генерации
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] Multy2Matrix(int[,] matr1, int[,] matr2) // метод перемножения двух матриц
+    {
+        int[,] resultMatrix = new int[matr1.GetLength(0),  matr1.GetLength(1)]; // создается результирующая матрица
+
+        for (int i = 0; i < matr1.GetLength(0); i++)
+        {
+            for (int j = 0; j < matr2.GetLength(1); j++)
+            {
+                for (int k = 0; k < matr2.GetLength(0); k++)
+                {
+                    resultMatrix[i, j] += matr1[i, k] * matr2[k, j];
+                }                
+            }            
+        }
+        return resultMatrix;
+    }
+
+
+int row = 3, colm = 3; // размерность для матриц
+int[,] matrix1 = new int[row, colm]; // обьявляется матрица и ее размерность
+int[,] matrix2 = new int[row, colm];
+int[,] multyMatrix = new int[row, colm];
+matrix1 = FillMatrix(matrix1); // заполнение массива случайными числами и возвращенеие результата
+matrix2 = FillMatrix(matrix2); 
+PrintMatrix(matrix1); // вывод матрицы на экран
+Console.WriteLine();
+PrintMatrix(matrix2);
+Console.WriteLine();
+multyMatrix = Multy2Matrix(matrix1,matrix2); // присвоение результатов перемножения двух матриц
+PrintMatrix(multyMatrix);
